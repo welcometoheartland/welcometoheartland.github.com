f0c1c17ca7365858b5a9a21d0793561aea688bef
welcometoheartland <wushujia5@gmail.com>
2021-12-07 03:44:41 -0500

    试试

---
+++ script.js
@@ -0,0 +1,29 @@
+$('.show-card').click(function(e) {
+  $('.card').addClass('show').css('display', 'block');
+  $('.show-card').addClass('hide');
+  $('.show-card1').addClass('hide');
+});
+
+$('.card .close').click(function(e) {
+  $('.card').addClass('hide');
+  setTimeout(function() {
+    $('.card').css('display', 'none').removeClass('show').removeClass('hide');
+  }, 600);
+  $('.show-card').removeClass('hide');
+  $('.show-card1').removeClass('hide');
+});
+
+$('.show-card1').click(function(e) {
+  $('.card1').addClass('show').css('display', 'block');
+  $('.show-card1').addClass('hide');
+  $('.show-card').addClass('hide');
+});
+
+$('.card1 .close1').click(function(e) {
+  $('.card1').addClass('hide');
+  setTimeout(function() {
+    $('.card1').css('display', 'none').removeClass('show').removeClass('hide');
+  }, 600);
+  $('.show-card1').removeClass('hide');
+  $('.show-card').removeClass('hide');
+});
